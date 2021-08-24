5fd456d23749492164865a9bec7b31f121aa5e97
Lukas <lukas.ancikevicius@gmail.com>
2021-08-24 09:02:24 +0300

    reducer demo incr decr

--- reducer-demo.js
+++ reducer-demo.js
@@ -2,4 +2,32 @@ console.log('reducer demo');
 
 const redux = require('redux');
 
-const store = redux.createStore();
+// reducer function
+const counterReducer = (state = { counter: 0 }, action) => {
+  //   console.log('inside reducer fn', state.counter);
+  if (action.type === 'INCR') {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === 'DECR') {
+    return { counter: state.counter - 1 };
+  }
+  return state;
+};
+
+// one source of truth
+const store = redux.createStore(counterReducer);
+
+// prisijungimo funkcija
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log('subscriber state', latestState);
+};
+
+// visi failai kurie nori naudoti store turi prisijungti su subscribe
+store.subscribe(counterSubscriber);
+
+// ivygdom reducerio funkcija su dispatch
+
+store.dispatch({ type: 'INCR' });
+store.dispatch({ type: 'INCR' });
+store.dispatch({ type: 'DECR' });
